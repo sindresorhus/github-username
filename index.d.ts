fe861dbe1a9788c5668e0d92ffb1be0a762f98f6
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-15 02:28:20 +0000

    Require Node.js 8, add TypeScript definition (#9)

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+/**
+Get a GitHub username from an email address.
+
+@param email - Email address for the user of whom you want the username.
+@param token - GitHub [personal access token](https://github.com/settings/tokens/new).
+@returns The username for the `email`.
+
+@example
+```
+import githubUsername = require('github-username');
+
+(async () => {
+	console.log(await githubUsername('sindresorhus@gmail.com'));
+	//=> 'sindresorhus'
+})();
+```
+*/
+declare function githubUsername(email: string, token: string): Promise<string>;
+
+export = githubUsername;
